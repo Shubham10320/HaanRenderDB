a2aca576cfc9606afa964e708feeaa68d453f041
Shubham10320 <chaubeyshubh1032@gmail.com>
2023-08-09 18:05:32 +0530

    rendering

---
+++ index.js
@@ -0,0 +1,15 @@
+const jsonServer = require("json-server"); // importing json-server library
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
+const port = process.env.PORT || 8080; //  chose port from here like 8080, 3001
+server.use(middlewares);
+server.use(router);
+
+server.listen(port);
+
+
+
+//npm i json-server 
+//npm init -y
+//json-server db.json --watch
